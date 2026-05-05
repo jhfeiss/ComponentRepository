0fd48e2066ec49a5acba9ea05f5edc858967fdca
jhfeiss <jakefeiss@icloud.com>
2026-05-05 12:03:16 -0400

    remove dist

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as react_jsx_runtime from 'react/jsx-runtime';
-
-type LoadingSpinnerProps = {
-    size?: number;
-    color?: string;
-};
-declare const LoadingSpinner: ({ size, color, }: LoadingSpinnerProps) => react_jsx_runtime.JSX.Element;
-
-export { LoadingSpinner };
